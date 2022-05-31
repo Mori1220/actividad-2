1e5676ab5ba30d0cfe1ccccf7e83eb66ab7f9b7e
Karla.Rodriguez <moriAeseces@gmail.com>
2022-05-30 21:47:21 -0500

    Feat: added function doSomething to index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function doSomething() { 
     console.log("Hello World");
 }
+
+function functionFromNewBranch() {
+    console.log ("Hello World from new branch");
+}
+
+function functionFromNewBranch() {
+    console.log ("Hello World from new branch");
+}
